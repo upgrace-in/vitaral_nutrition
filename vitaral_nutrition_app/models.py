5ed8f02765dec37b1c616a43e28e340682d1ea88
itzkartik <smartzboi@gmail.com>
2020-06-11 10:40:36 +0530

    make

--- vitaral_nutrition_app/models.py
+++ vitaral_nutrition_app/models.py
@@ -63,10 +63,12 @@ class my_acc_info(models.Model):
 class timer_info(models.Model):
     timer = models.IntegerField()
 
-class analytics(models.Model):
+class analytic_model(models.Model):
+    home_page = models.IntegerField()
     initial_page = models.IntegerField()
     how_it_work_page = models.IntegerField()
-    home_page = models.IntegerField()
+    thank_you = models.IntegerField()
+
 
 
 
