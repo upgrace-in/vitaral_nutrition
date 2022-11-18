7ca13dff3da785533ae98d9aa377bb7d3621f13e
ItzKartik <smartzboi@gmail.com>
2022-11-18 17:14:13 +0100

    work

--- vitaral_nutrition/settings.py
+++ vitaral_nutrition/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')i@)mew^on6exks-u0*s$p@x0srdd!n-bu!t5a_pjse0!)fv^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
